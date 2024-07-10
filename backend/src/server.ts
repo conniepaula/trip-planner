485ad8e0b379c5cd6d33884a559c677f0b9fc123
Connie <conniedpaula@gmail.com>
2024-07-10 17:50:53 +0100

    feat: add get trip details, update trip and get participant routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,6 +14,9 @@ import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
 import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
 
 const app = fastify();
 
@@ -24,11 +27,14 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(updateTrip);
+app.register(getTripDetails);
 app.register(confirmParticipant);
 app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipant);
 app.register(getParticipants);
 app.register(createInvite);
 
